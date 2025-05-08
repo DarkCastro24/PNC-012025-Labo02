627a60cb2e896ee2e07c16804da67ab45f77203a
Alison Argueta <105072904+AlisonArgue@users.noreply.github.com>
2025-05-07 21:18:37 -0600

    :sparkles: Entity puesto con empleado

--- PNC-Laboratorio02/src/main/java/org/example/pnclaboratorio02/Domain/Entities/Empleado.java
+++ PNC-Laboratorio02/src/main/java/org/example/pnclaboratorio02/Domain/Entities/Empleado.java
@@ -25,10 +25,11 @@ public class Empleado {
     @Column(nullable = false)
     private String fechaIngreso;
 
-    @Column(nullable = false)
-    private String puesto;
-
     @ManyToOne
     @JoinColumn(name = "id_departamento", nullable = false, foreignKey = @ForeignKey(name = "Fk_user_departamento"))
     private Departamento departamento;
+
+    @ManyToOne
+    @JoinColumn(name = "id_puesto", nullable = false, foreignKey = @ForeignKey(name = "Fk_user_puesto"))
+    private Puesto puesto;
 }
